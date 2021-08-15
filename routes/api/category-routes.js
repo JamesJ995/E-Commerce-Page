8e7c2233286c3f07f091474aa31f2c682a0ffb0c
jamesj995 <mrjam7@gmail.com>
2021-08-15 02:39:59 -0500

    fixing files

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,22 +1,20 @@
-const router = require("express").Router();
-const { Category, Product } = require("../../models");
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll({
-      include: [{ model: Product }],
-    });
+    const categoryData = await Category.findAll({});
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -26,7 +24,7 @@ router.get("/:id", async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: "No location found with this id!" });
+      res.status(404).json({ message: 'No location found with this id!' });
       return;
     }
 
@@ -36,7 +34,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
@@ -46,7 +44,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   // update a category by its `id` value
   Category.update(
     {
@@ -65,7 +63,7 @@ router.put("/:id", (req, res) => {
     .catch((err) => res.json(err));
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
@@ -75,7 +73,7 @@ router.delete("/:id", async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: "No location found with this id!" });
+      res.status(404).json({ message: 'No location found with this id!' });
       return;
     }
 
